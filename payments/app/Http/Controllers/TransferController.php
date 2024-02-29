d42facb1c6c467884255413efc8b9da4b089d263
A1034512 <lucasrodrigues@educarbrasil.org.br>
2024-02-29 12:32:46 -0300

    deletando funções não usadas

--- payments/app/Http/Controllers/TransferController.php
+++ payments/app/Http/Controllers/TransferController.php
@@ -10,7 +10,6 @@ use App\Repositories\UserRepository;
 use App\Repositories\WalletRepository;
 use Exception;
 use Illuminate\Support\Facades\Http;
-use App\Models\Transfer;
 use Illuminate\Http\Request;
 
 class TransferController extends Controller
@@ -105,61 +104,5 @@ class TransferController extends Controller
         }
 
         return $this->return;
-    }
-
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Transfer $transfer)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Transfer $transfer)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Transfer $transfer)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Transfer $transfer)
-    {
-        //
     }
 }
